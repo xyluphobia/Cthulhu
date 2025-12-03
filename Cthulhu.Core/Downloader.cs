e6b42f8d576a877f0b61a6a96155e0acb84f9e2f
Matthew <mattscavone11@gmail.com>
2025-12-04 10:55:05 +1100

    Moving appartments

--- Cthulhu.Core/Downloader.cs
+++ Cthulhu.Core/Downloader.cs
@@ -3,18 +3,22 @@
 public class Downloader
 {
   HttpClient httpClient = new HttpClient();
-  string TEMP_downloadRoute = @"C:Users\Matthew\Downloads\download.txt";
+  string TEMP_downloadRoute = @"C:Users\Matthew\Downloads\";
   
   public async Task Download(Uri uri) {
     try {
       Console.WriteLine("Validating Hosted File..");
-      HttpResponseMessage response = await httpClient.GetAsync(uri);
+      HttpResponseMessage response = await httpClient.GetAsync(
+        uri,
+        HttpCompletionOption.ResponseHeadersRead
+      );
       response.EnsureSuccessStatusCode();
+      string fileName = Path.GetFileName(uri.LocalPath);
       
       Console.WriteLine("File Located Online");
       Stream stream = await response.Content.ReadAsStreamAsync();
       FileStream fileStream = new FileStream(
-        path: TEMP_downloadRoute, 
+        path: TEMP_downloadRoute + fileName, 
         mode: FileMode.Create, 
         access: FileAccess.Write
       );
---
+++ Cthulhu.Core/Settings/UserSettings.cs
@@ -0,0 +1,7 @@
+using System.Text.Json.Serialization;
+
+
+public sealed class UserSettings {
+  public string? downloadDirectory { get; set; } = null;
+
+}
